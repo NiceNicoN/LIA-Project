b766a6659eb2153a81beac35316b6ebdf6ba4f86
NiceNicoN <151521290+NiceNicoN@users.noreply.github.com>
2023-12-11 21:57:19 -0500

    Update dec4c_ObstacleAvoidance.ino

--- dec4c_ObstacleAvoidance.ino
+++ dec4c_ObstacleAvoidance.ino
@@ -1,5 +1,13 @@
+//* Obstacle Avoidance *//
+
+//This code uses the HC-SR04 Ultasonic Sensor to measure distance.//
+//Here it is paired with a SG90 Servo Motor to help it detect     //
+//obstacles in a field of 180°. This in turn helps the Elegoo Car //
+//avoid collisions, avoid and back out of corners.                //
+
+
 //Include the Servo Motor Library
-#include <Servo.h>
+#include <Servo.h>  //This library contains the codes necessary to use the SG90 Servo Moror
 
 //Create the Servo Object to control the Servo Motor
 Servo myservo;
@@ -14,30 +22,28 @@ int STBY = 3;
 
 // Pins on the Arduino that belong to the motors' of the wheels // 
 
-//Motor A
-int PWMA = 5;    
-int AIN1 = 8;    
+//Motor A  (Motors on the right side of the car)
+int PWMA = 5;    // Pin 5 controls how fast Motor A is spinning
+int AIN1 = 8;    // Pin 8 controls in which direction (forward or backward) Motor A is spinning
 
-//Motor B
-int PWMB = 6;    
-int BIN1 = 7;
-  
-// Recieving Pin for Infra Red
-int IR_PIN = 9;
+//Motor B  (Motors on the left side of the car)
+int PWMB = 6;    // Pin 6 controls how fast Motor B is spinning
+int BIN1 = 7;    // Pin 7 controls in which direction (forward or backward) Motor B is spinning
  
 // Pins for the Ultrasonic Sensor
-int trigPin = 13; 
-int echoPin = 12;
+int trigPin = 13;           //Pin 13, the Trigger pin, is the one driven to send out the ultrasonic pulses
+int echoPin = 12;           //Pin 12, the Echo in, is the one that produces a pulse when the reflected signal is received.
 
-float duration, distance;
+// Precise Variables
+float duration, distance;   //With float, the duration and distace variables become precise to the decimal
 
-// Available Speeds //  
+// Available Speeds //    These will be the the Y variables when a command is called
 int MaxSPEED= 250;   
 int MinSPEED= 50;    
 int NormSPEED= 100;    
 int DontMOVE= 0;  
 
-// Available Times //
+// Available Times //     These will be the the X variables when a command is called)
 int Stop = 0; 
 int Time = 1000;  
 int IRResponseTime = 0; 
@@ -46,43 +52,59 @@ int IRResponseTime = 0;
 
 void Forward(int x,int y){  //Forward command  
   //motor A    
-  analogWrite (PWMA,100);    
-  digitalWrite (AIN1,1);    
+  analogWrite (PWMA,y);     //Motor A is powered at speed y
+  digitalWrite (AIN1,1);    //The comand of 1(HIGH) makes motor A move forward
 
   //Motor B      
-  analogWrite(PWMB, 100); 
-  digitalWrite (BIN1,1);    
+  analogWrite (PWMB, y);    //Motor A is powered at speed y
+  digitalWrite (BIN1,1);    //The comand of 1(HIGH) makes motor B move forward
 } 
 
 void Backward(int x,int y){ //Backward command  
   //motor A    
-  analogWrite (PWMA,y);    
-  digitalWrite (AIN1,0);
+  analogWrite (PWMA,y);     //Motor A is powered at speed y
+  digitalWrite (AIN1,0);    //The comand of 0(LOW) makes motor A move backward
 
   //Motor B
-  analogWrite(PWMB, y);
-  digitalWrite (BIN1,0);
+  analogWrite (PWMB, y);    //Motor B is powered at speed y
+  digitalWrite (BIN1,0);    //The comand of 0(LOW) makes motor B move backward
 }   
 
 void Left(int x,int y){ //Left command  
   //motor A    
-  analogWrite (PWMA,y);    
-  digitalWrite (AIN1,1);
+  analogWrite (PWMA,y);     //Motor A is powered at speed y
+  digitalWrite (AIN1,0);    //The comand of 0(LOW) makes motor A move backward
 
   //Motor B    
-  analogWrite(PWMB, y);    
-  digitalWrite (BIN1,0); 
+  analogWrite(PWMB, y);     //Motor B is powered at speed y
+  digitalWrite (BIN1,1);    //The comand of 1(HIGH) makes motor B move forward
+
+  // In the case of the Left command, Motor A is moving backward at full speed and Motor B is moving forwards at full speed
+  // causing the Car to spin to the Left in the same spot.
 }   
 
-void Right(int x,int y){ //Right command  
+void Right(int NormSPEED){ //Right command  
   //motor A    
-  analogWrite (PWMA,y);    
-  digitalWrite (AIN1,0);    
+  analogWrite (PWMA, NormSPEED);     
+  digitalWrite (AIN1,1);    
 
   //Motor B    
-  analogWrite(PWMB, y);    
-  digitalWrite (BIN1,1);    
-}   
+  analogWrite(PWMB, NormSPEED);    
+  digitalWrite (BIN1,0);
+}
+
+//void Right(int x,int y){ //Right command  
+  //motor A    
+  //analogWrite (PWMA,y);     //Motor A is powered at speed y
+  //digitalWrite (AIN1,1);    //The comand of 1(HIGH) makes motor A move forward
+
+  //Motor B    
+  //analogWrite(PWMB, y);     //Motor B is powered at speed y
+  //digitalWrite (BIN1,0);    //The comand of 0(LOW) makes motor B move backward
+
+  // In the case of the Right command, Motor A is moving forward at full speed and Motor B is moving backwards at full speed
+  // causing the Car to spin to the Right in the same spot.
+//}   
 
 void LeftForward(int x,int y){   //Left Forward command
   //motor A  
@@ -126,13 +148,11 @@ void RightBackward(int x,int y){  //Right Backward command
 
 void Standby(){ //Standby command  
   //motor A     
-  analogWrite (PWMA,0); 
-  digitalWrite (AIN1,1);     
+  analogWrite (PWMA,0);     //Motor A is powered at speed 0
 
   //Motor B       
-  analogWrite(PWMB,0);     
-  digitalWrite (BIN1,1);       
-}  
+  analogWrite (PWMB,0);     //Motor B is powered at speed 0
+}       
 
 void Ultrasonic(){  //Ultrasonic command
   // Write a pulse to the HC-SR04 Trigger Pin 
@@ -184,46 +204,55 @@ void loop() {
   //if(LT_L >=1000 && LT_M >=1000 && LT_R >=1000){ 
    // Standby();   
     //Serial.println("STOP");
- // }
+  // }
   
-  if (distance > 20){
-    Forward(0,NormSPEED);
-    myservo.write(90);
-    Ultrasonic();
-  }
+  if (distance <= 20){   //if true that the object is less than 20 cm in front of the car, run next line of code.
+    Standby();            ////Stop car
+    myservo.write(0);     ////Servo turn Right
+    delay(200);            ///for 0.2 seconds
+    Ultrasonic();         ////Run Ultrasonic command
+    delay(200);            ///for 0.2 seconds
 
-  else if (distance < 20){
-    Standby();
-    delay(1000);
-    myservo.write(0);
-    delay(1000);
-    Ultrasonic();
+    if (distance <= 20){   //if true that the object is less than 20 cm to the right of the car, run next line of code.
+      Standby();            ////Car Stops
 
-    if (distance < 20){
-     Right(3000,NormSPEED);
-     myservo.write(90);
+      myservo.write(180);   ////Servo turn Left
+      delay(200);            ///for 0.2 seconds
+      Ultrasonic();         ////Run Ultrasonic command
+      delay(200);            ///for 0.2 seconds
+    
+      if (distance > 20){                  //if false that the object is less than 20 cm to the left of the car, run next line of code.
+        Left(0,NormSPEED);    ////Car turns Left, at 100 speed
+        delay(500);            ///for 0.5 seconds
+        myservo.write(90);    ////Servo reajusts by turning Forward
+      }
+
+      else{   //if true that the object is less than 20 cm to the left of the car, run next line of code.
+        Standby();
+        delay(200);
+        Backward(0,NormSPEED);////Car goes Backwards, at 100 speed
+        delay(500);            ///for 0.5 seconds
+        Left(0,NormSPEED);   ////Car turns Right, at 100 speed
+        delay(500);           ///for 1 second
+        
+        myservo.write(90);    ////Servo reajusts by turning Forward
+        delay(200);            ///for 0.2 seconds
+        Ultrasonic();         ////Run Ultrasonic command
+        delay(200);            ///for 0.2 seconds
+      }
     }
     
-    else if (distance < 20){
-     Standby();
-     delay(1000);
-     myservo.write(180);
-     delay(100);
-     Ultrasonic();
-     delay(250);
-     Ultrasonic();
-     
-      if (distance > 20){
-       Left (750,NormSPEED);
-       myservo.write(90);
-      }
-      else if( distance < 20){
-       Backward (500,NormSPEED);
-       Left(1400,NormSPEED);
-       myservo.write(90);
-      }
-     }
+    else{                  //if false that the object is less than 20 cm to the right of the car, run next line of code.
+      Right(0,NormSPEED);   ////Car turns Right, at 100 speed
+      delay(500);            ///for 0.5 seconds
+      myservo.write(90);    ////Servo reajusts by turning Forward
+    }
   }
   
+  else{                  //if false that the object is less than 20 cm in front of the car, run next line of code.
+    Forward(0,NormSPEED); ////Car goes Forward, at 100 speed
+    myservo.write(90);    ////Servo reajusts by turning Forward
+    Ultrasonic();         ////Run Ultrasonic command
+    delay(100);            ///for 0.1 seconds
+  }
 }
-  
